b73d3e3f3d7a796f1cb19991eb682ef2cc05de16
hmbashar <hmbashar@gmail.com>
2022-09-14 05:26:42 +0600

    faq class changed

--- assets/js/faq-script.js
+++ assets/js/faq-script.js
@@ -1,21 +1,21 @@
 (function($){
 	$(document).ready(function(){
 	//First child show
-	$('#aefe_accordion').children('.single_accordion:nth-child(2)').children('.accordion_content').slideDown();
+	$('#aefe_accordion').children('.aefe-faq-1-single_accordion:nth-child(2)').children('.aefe-faq-1-accordion_content').slideDown();
 	
 	//first child add active class
-	$('#aefe_accordion').children('.single_accordion:nth-child(2)').addClass('active_single_accordion').children('.accordion_hearder').addClass('accordion_active');
+	$('#aefe_accordion').children('.aefe-faq-1-single_accordion:nth-child(2)').addClass('aefe-faq-1-active_single_accordion').children('.aefe-faq-1-accordion_hearder').addClass('aefe-faq-1-accordion_active');
 	
 	//Main Accordion
-	$('.accordion_hearder').click(function(){
+	$('.aefe-faq-1-accordion_hearder').click(function(){
 	  //Accordion Content Toggle Slide
-	  $(this).next('.accordion_content').slideToggle()
+	  $(this).next('.aefe-faq-1-accordion_content').slideToggle()
 	  //add class
-	  .toggleClass('accordion_active')
+	  .toggleClass('aefe-faq-1-accordion_active')
 	  //When click any accordion other content slide up.
-	  .parents('.single_accordion').toggleClass("active_single_accordion").siblings().removeClass("active_single_accordion").children('.accordion_content').slideUp()
+	  .parents('.aefe-faq-1-single_accordion').toggleClass("aefe-faq-1-active_single_accordion").siblings().removeClass("aefe-faq-1-active_single_accordion").children('.aefe-faq-1-accordion_content').slideUp()
 	  //Active class remove other accordion header
-	  .prev('.accordion_hearder').removeClass('accordion_active');
+	  .prev('.aefe-faq-1-accordion_hearder').removeClass('aefe-faq-1-accordion_active');
 	  
 	});
 		
--- elementor-addon/includes/widgets/faq/faq-style-one.php
+++ elementor-addon/includes/widgets/faq/faq-style-one.php
@@ -5,48 +5,48 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-<div class="freeask-accordian-area">
-	<div class="accordion_area " id="aefe_accordion">
+<div class="aefe-faq-1-freeask-accordian-area">
+	<div class="aefe-faq-1-accordion_area " id="aefe_accordion">
         <!--Single Accordion-->
-        <div class="single_accordion ">
-            <div class="accordion_hearder fix">
+        <div class="aefe-faq-1-single_accordion ">
+            <div class="aefe-faq-1-accordion_hearder fix">
                 <h2><span>01.</span>  Sedeiusmod tempor inccsetetur aliquatraiy?</h2>
-                <span class="accordion_icon">
+                <span class="aefe-faq-1-accordion_icon">
                     <i class="fa fa-plus"></i>
                     <i class="fa fa-minus"></i>
                 </span>
             </div>
-            <div class="accordion_content fix">
+            <div class="aefe-faq-1-accordion_content fix">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporo incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrd exercitation ullamco laboris nisi ut aliquip.</p>
             </div>
         </div>	<!--/ Single Accordion-->
         <!--Single Accordion-->
 
         <!--Single Accordion-->
-        <div class="single_accordion ">
-            <div class="accordion_hearder fix">
-                <h2><span>01.</span>  Sedeiusmod tempor inccsetetur aliquatraiy?</h2>
-                <span class="accordion_icon">
+        <div class="aefe-faq-1-single_accordion ">
+            <div class="aefe-faq-1-accordion_hearder fix">
+                <h2><span>02.</span>  Sedeiusmod tempor inccsetetur aliquatraiy?</h2>
+                <span class="aefe-faq-1-accordion_icon">
                     <i class="fa fa-plus"></i>
                     <i class="fa fa-minus"></i>
                 </span>
             </div>
-            <div class="accordion_content fix">
+            <div class="aefe-faq-1-accordion_content fix">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporo incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrd exercitation ullamco laboris nisi ut aliquip.</p>
             </div>
         </div>	<!--/ Single Accordion-->
         <!--Single Accordion-->
 
         <!--Single Accordion-->
-        <div class="single_accordion ">
-            <div class="accordion_hearder fix">
-                <h2><span>01.</span>  Sedeiusmod tempor inccsetetur aliquatraiy?</h2>
-                <span class="accordion_icon">
+        <div class="aefe-faq-1-single_accordion ">
+            <div class="aefe-faq-1-accordion_hearder fix">
+                <h2><span>03.</span>  Sedeiusmod tempor inccsetetur aliquatraiy?</h2>
+                <span class="aefe-faq-1-accordion_icon">
                     <i class="fa fa-plus"></i>
                     <i class="fa fa-minus"></i>
                 </span>
             </div>
-            <div class="accordion_content fix">
+            <div class="aefe-faq-1-accordion_content fix">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporo incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrd exercitation ullamco laboris nisi ut aliquip.</p>
             </div>
         </div>	<!--/ Single Accordion-->
